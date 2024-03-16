29b42c318bf3ab07b6a94876579dbaf316376961
Slava Chalnev <slava.chalnev@gmail.com>
2024-03-16 17:52:52 +0700

    Bert training

--- tiny_bert/model.py
+++ tiny_bert/model.py
@@ -6,14 +6,16 @@ from transformer_lens import HookedTransformer
 def get_config_and_tok(bidirectional=True):
     causal_model = HookedTransformer.from_pretrained("attn-only-2l")
     config = causal_model.cfg.to_dict()
-
     config['init_weights'] = True
-
     config['attention_dir'] = 'bidirectional' if bidirectional else 'causal'
-    return config, causal_model.tokenizer
+
+    config['d_vocab'] = config['d_vocab'] + 1 # add mask token
+    tokenizer = causal_model.tokenizer
+    tokenizer.add_special_tokens({'mask_token': "<MASK>"}) # tok id 48262
+
+    return config, tokenizer
 
 
-
-
-
+# config, tok = get_config_and_tok()
+# print('done')
 
--- tiny_bert/train.py
+++ tiny_bert/train.py
@@ -5,25 +5,49 @@
 import torch
 from torch.utils.data import DataLoader
 from transformer_lens import HookedTransformer
+from datasets import load_dataset
 from model import get_config_and_tok
 
+
+def mask_tokens(inputs, tokenizer, mlm_probability=0.15):
+    labels = inputs.clone()
+    probability_matrix = torch.full(labels.shape, mlm_probability)
+    masked_indices = torch.bernoulli(probability_matrix).bool()
+    labels[~masked_indices] = -100  # We only compute loss on masked tokens
+
+    # 80% of the time, we replace masked input tokens with tokenizer.mask_token ([MASK])
+    indices_replaced = torch.bernoulli(torch.full(labels.shape, 0.8)).bool() & masked_indices
+    inputs[indices_replaced] = tokenizer.mask_token_id
+
+    # 10% of the time, we replace masked input tokens with random word
+    indices_random = torch.bernoulli(torch.full(labels.shape, 0.5)).bool() & masked_indices & ~indices_replaced
+    random_words = torch.randint(tokenizer.vocab_size, labels.shape, dtype=torch.long, device=inputs.device)
+    inputs[indices_random] = random_words[indices_random]
+
+    # The rest of the time (10% of the time) we keep the masked input tokens unchanged
+    return inputs, labels
 
 
 def train(model: HookedTransformer, dataloader: DataLoader, device):
     model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
-
     for batch in dataloader:
-        inputs = batch["input"].to(device)
-        targets = batch["target"].to(device)
+        inputs = batch["tokens"].to(device)
+
+        inputs, labels = mask_tokens(inputs, model.tokenizer)
 
         optimizer.zero_grad()
         outputs = model(inputs)
-
+        
         # Bert's masked language model loss
-        loss = None # todo
-
+        loss = torch.nn.functional.cross_entropy(
+            outputs.view(-1, model.tokenizer.vocab_size),
+            labels.view(-1),
+            ignore_index=-100,
+        )
+        
         loss.backward()
+        print(loss.item())
         optimizer.step()
 
 
@@ -33,9 +57,13 @@ def main():
     model = HookedTransformer(config, tokenizer, move_to_device=False)
     model.to(device)
 
-    # Prepare your dataset and dataloader
-    dataset = None  # Load your dataset
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    # data is already tokenized and is in chunks of 1024 token ids.
+    data = load_dataset("NeelNanda/c4-code-tokenized-2b", split="train[:10%]")
+    data.set_format(type="torch", columns=["tokens"])
+    print('loaded data')
+
+    dataloader = DataLoader(data, batch_size=32, shuffle=True)
+    print('initialized dataloader')
 
     train(model, dataloader, device)
 
